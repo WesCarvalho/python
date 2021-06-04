1ceca0acf6638a972683b7c12ad4d66f0769eee0
WesCarvalho <weslley_lopes@hotmail.com>
2021-06-04 10:22:38 -0300

    Variáveis e tipos

---
+++ main.py
@@ -0,0 +1,23 @@
+print('Ola mundo do python!')
+
+temperatura = 20
+print(f'tipo int:{temperatura}')
+
+ListaNomes = ['Weslley', 'José', 'Cesar']
+print(f'Tipo lista: {ListaNomes}')
+
+dicionarioPessoa = {
+    'nome': 'Weslley',
+    'idade': '30'
+}
+print(f"Tipo dicionario: {dicionarioPessoa}")
+
+verificacao = True
+print(verificacao)
+
+variavelNula = None
+
+# comentario no python
+
+numero = input("Digite um numero:")
+print(f'O numero digitado foi {numero}')
